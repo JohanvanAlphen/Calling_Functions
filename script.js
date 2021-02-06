be32acda54d7a67578cfccfbc2e970826f4df2be
Johan van Alphen <johanvanalphen@hotmail.com>
2021-02-06 14:03:06 +0100

    solution for Function Basics

---
+++ script.js
@@ -0,0 +1,28 @@
+const makePizza = function () {
+  console.log("Make dough");
+  console.log("Flatten the dough");
+  console.log("Put tomato sauce, cheese, peppers and mushrooms on it");
+  console.log("Put the pizza in the oven for 20 minutes at 180 degrees");
+  console.log("");
+}
+const makeSushi = function () {
+  console.log("Prep the Rice & Nori");
+  console.log("Spread the Rice on the Nori");
+  console.log("Add Fillings to the Bottom Quarter of the Nori");
+  console.log("Roll the Sushi Part Way");
+  console.log("Roll the Sushi the Rest of the Way");
+  console.log("Move to Cutting Board");
+  console.log("Cut the Sushi");
+  console.log("Serve...");
+  console.log("");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
+
+
