9601153c38c4e3f28c9d5736f70f2319fabb36dc
Johan van Alphen <johanvanalphen@hotmail.com>
2021-02-09 17:36:22 +0100

    my solutions for Return Statements

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-// No arguments
+// Checking if number > 100
 
-const paintWallRed = function () {
-  console.log("The wall has been painted red")
-}
-paintWallRed();
+const checkNumber = function (number1, number2) {
+  const sum = number1 * number2
+  if (sum < 100) {
+    return true
+  }
+  return false
+};
 
-// Single argument
+const result1 = checkNumber(21, 5);
+const result2 = checkNumber(3, 4);
+console.log("Your result is", result1);
+console.log("Your result is", result2)
 
-const paintWall = function (item) {
-  console.log("The wall has been painted", item)
-}
-paintWall("green");
-paintWall("blue");
+// Above mentioned function does something, the result is true or false (boolean)
 
-// Multiple arguments
+// Bouncer at a club
 
-const northWall = "The north wall has been painted"
-const southeastWall = "The south-east wall has been painted"
-const paintWallOrange = function (item) {
-  console.log(northWall, item);
-}
-const paintWallGrey = function (item) {
-  console.log(southeastWall, item);
-}
+const allowedInClub = function (maxNumberPeople, currentNumberPeople, minAgePerson) {
 
-paintWallOrange("orange");
-paintWallGrey("grey");
+  if ((currentNumberPeople < maxNumberPeople) && (minAgePerson => 21)) {
+    return "come in"
+  }; if (currentNumberPeople > maxNumberPeople) {
+    return "it's too busy now, come back later"
+  }; if (minAgePerson < 21)
+    return "this is a club for adults"
+};
+
+const person1 = allowedInClub(1900, 1500, 24);
+const person2 = allowedInClub(1900, 2000, 24);
+const person3 = allowedInClub("", "", 18);
+console.log("Answer person 1", person1);
+console.log("Answer person 2", person2);
+console.log("Answer person 3", person3);
+
+// Calculate the average
+
+const calculateAverage = function (num1, num2, num3, num4, num5) {
+  const sum = Math.round((num1 + num2 + num3 + num4 + num5) / calculateAverage.length);
+  return sum;
+};
+
+const result = calculateAverage(6, 8, 12, 4, 5);
+console.log("The (rounded) average is", result)
+
+
+
+// const array = [5, 8, 9, 3, 6]
+// const sum = array.reduce((previous, current) => current += previous);
+// const calculateAverage = Math.round(sum / array.length);
+// console.log("The (rounded) average is", calculateAverage)
+
+
